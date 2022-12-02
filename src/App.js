f759e7f4a10d3effa0d3a3e0c48d670e45ee938e
mustafadarras <118083791+mustafadarras@users.noreply.github.com>
2022-12-01 23:43:42 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <head>
+        <title>LAB10C</title>
+      </head>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>How I installed React On My Computer</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        First, I went to "www.nodejs.org". After going into the webiste,
+        I downloaded a 64 bit version of the "Node js" to my computer.
+        I then installed it on my computer, then created a folder on my 
+        C drive that is called "react". 
+        Next, I opened Windows powershell as administrator, then on powershell
+        I changed my directory to the location of my react folder, then 
+        executed the command "npm init reac-app reactjs", once the installation was downloaded
+        I opened the folder "reactjs" using Visual Studio Code to start editing my website.
+        then I went to my reactjs folder using the cd command and then I executed the command
+        "npm start". Finally, I was able to change and design my Lab10C website.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        
       </header>
     </div>
   );
